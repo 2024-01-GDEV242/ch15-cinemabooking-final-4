6208b888fea521d4e5e6730e973f81632f57e728
Alp <hizalper@gmail.com>
2024-05-05 01:09:33 -0400

    Seat implementation.

--- Seat.java
+++ Seat.java
@@ -1,15 +1,13 @@
-
 /**
- * The class represents a single seat.
- *
- * @author Juan Jimenez
- * @version 4/29/24
+ * Represents a single seat.
+ * 
+ * @Author Alper Hiz
+ * version 5/5/2024  
  */
-public class Seat
-{
+public class Seat {
     private int seatNumber;
-    private boolean isBooked;
-    
+    private boolean booked;
+
     /**
      * Constructs a seat with a seat number.
      * 
@@ -17,23 +15,60 @@ public class Seat
      */
     public Seat(int seatNumber) {
         this.seatNumber = seatNumber;
-        this.isBooked = false;
+        this.booked = false;
     }
-    
+
     /**
-     * Books the seat.
-     */
-    public void bookSeat() {
-        this.isBooked = true;
-    }
-    
-    /**
-     * Checks if the seat is already booked.
+     * Books the seat if it's available.
      * 
-     * @return true if the seat is booked
+     * @return true if the seat was successfully booked, false if it was already booked
+     */
+    public boolean bookSeat() {
+        if (!booked) {
+            booked = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if the seat is booked.
+     * 
+     * @return true if the seat is booked, false otherwise
      */
     public boolean isBooked() {
-        return isBooked;
+        return booked;
+    }
+
+    /**
+     * Gets the seat number.
+     * 
+     * @return The seat number
+     */
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    /**
+     * Sets the seat number.
+     * 
+     * @param seatNumber The seat number to set
+     */
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    /**
+     * Returns a string representation of the seat.
+     * 
+     * @return A string representation of the seat
+     */
+    @Override
+    public String toString() {
+        return "Seat{" +
+                "seatNumber=" + seatNumber +
+                ", booked=" + booked +
+                '}';
     }
 }
-
