f424f8543bf45adcc1694c73150e2e6333a23b72
Alp <hizalper@gmail.com>
2024-05-05 00:09:22 -0400

    Movie class implementation.

--- Movie.java
+++ Movie.java
@@ -1,19 +1,80 @@
-
 /**
- * A class that stores seating details relating to a movie.
- *
- * @author Juan Jimenez
- * @version 4/29/24
+ * A class that stores information about a movie.
+ * 
+ * @Author Alper Hiz
+ * version 5/5/2024
  */
-public class Movie
-{
-    private int x;
+public class Movie {
+    private String title;
+    private String director;
+    private String genre;
 
     /**
-     * Constructor for objects of class Movie
+     * Constructs a Movie object with the given title, director, and genre.
+     * 
+     * @param title    The title of the movie
+     * @param director The director of the movie
+     * @param genre    The genre of the movie
      */
-    public Movie()
-    {
-        
+    public Movie(String title, String director, String genre) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
     }
+
+    /**
+     * Gets the title of the movie.
+     * 
+     * @return The title of the movie
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Sets the title of the movie.
+     * 
+     * @param title The title of the movie
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Gets the director of the movie.
+     * 
+     * @return The director of the movie
+     */
+    public String getDirector() {
+        return director;
+    }
+
+    /**
+     * Sets the director of the movie.
+     * 
+     * @param director The director of the movie
+     */
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    /**
+     * Gets the genre of the movie.
+     * 
+     * @return The genre of the movie
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * Sets the genre of the movie.
+     * 
+     * @param genre The genre of the movie
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+
 }
