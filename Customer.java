255de89225d9ee9bc49bdc1d555286dd4d2abfa8
Alp <hizalper@gmail.com>
2024-05-05 00:24:18 -0400

    Customer class implementation.(Getters,setter, toString etc.)

--- Customer.java
+++ Customer.java
@@ -1,23 +1,70 @@
-
 /**
- * The class that store's customer details.
- *
- * @author Juan Jimenez
- * @version 4/29/24
+ * Represents a customer making a booking.
+ * 
+ * @Author Alper Hiz
+ * version 5/5/2024 
  */
-public class Customer
-{
+public class Customer {
     private String name;
     private String phoneNumber;
-    
+
     /**
-     * Constructs a customer with a name and phone number.
+     * Constructs a customer with the given name and phone number.
      * 
-     * @param name The customer's name
+     * @param name        The customer's name
      * @param phoneNumber The customer's phone number
      */
     public Customer(String name, String phoneNumber) {
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * Gets the customer's name.
+     * 
+     * @return The customer's name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the customer's name.
+     * 
+     * @param name The customer's name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the customer's phone number.
+     * 
+     * @return The customer's phone number
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Sets the customer's phone number.
+     * 
+     * @param phoneNumber The customer's phone number to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * Returns a string representation of the customer.
+     * 
+     * @return A string representation of the customer
+     */
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
     }
 }
